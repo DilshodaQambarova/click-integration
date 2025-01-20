249de86ed8138106973b4a26191b4cd6e270232a
Dilshoda <qambarovadilshoda867@gmail.com>
2025-01-20 22:15:34 +0500

    Removed route sms

--- routes/api.php
+++ routes/api.php
@@ -7,5 +7,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-Route::post('/sms', [SmsController::class, 'getToken']);
 Route::post('/send', [SmsController::class, 'sendSms']);
