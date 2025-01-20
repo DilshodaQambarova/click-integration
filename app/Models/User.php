0c90d31678db5dfe5e76d69bf87709ba3fca1359
Dilshoda <qambarovadilshoda867@gmail.com>
2025-01-20 22:36:25 +0500

    Added fillable

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'phone',
         'password',
     ];
 
@@ -43,6 +44,7 @@ class User extends Authenticatable
     {
         return [
             'email_verified_at' => 'datetime',
+            'phone_verified_at',
             'password' => 'hashed',
         ];
     }
