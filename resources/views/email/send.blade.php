1f584b17dc0b3f1301ce1413fe59aa046be70aae
Dilshoda <qambarovadilshoda867@gmail.com>
2025-01-22 20:14:28 +0500

    Added html code for send sms to mail

--- resources/views/email/send.blade.php
+++ resources/views/email/send.blade.php
@@ -1,3 +1,4 @@
 <div>
-    <!-- Act only according to that maxim whereby you can, at the same time, will that it should become a universal law. - Immanuel Kant -->
+    <h1>Welcome, {{$name}}!</h1>
+    <a href="{{$link}}"><h3>Email verification</h3></a>
 </div>
